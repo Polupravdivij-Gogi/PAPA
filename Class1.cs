6fcbc6fbbdece1a5b3b94e375537790c687a1676
Polupravdivij-Gogi <belyjnonemonarhist@gmail.com>
2023-03-13 10:36:46 +0300

    Сообщение

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP
+{
+    internal class Firma
+    {
+        private string name;
+        private double area;
+        private double lenght;
+
+        public string Name
+        {
+            get
+            {
+                return name; 
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+            set
+            {
+                area = value;
+            }
+        }
+
+        public double Lenght
+        {
+            get
+            {
+                return lenght;
+            }
+            set
+            {
+                lenght = value;
+            }
+        }
+
+        public Firma() {
+            name = "";
+            area = 0;
+            lenght = 0;
+        }
+
+
+
+        public Firma(string n, double a, double l)
+        {
+            name = n;
+            area = a;
+            lenght = l;
+        }
+
+        public void Set_ALL(string n, double a, double l)
+        {
+            name = n;
+            area = a;
+            lenght = l;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Firma firma = new Firma(); 
+            Console.WriteLine(firma.Name);
+            Console.ReadKey(true);
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
